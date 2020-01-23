b2377449419b21dba170f3d4618c88741ae23420
Cristopher Rodriguez <59444688+brogrammerJesus@users.noreply.github.com>
2020-01-23 17:04:48 -0500

    list of tunes in objects.  used a constructor function

---
+++ tuneObject.js
@@ -0,0 +1,269 @@
+"use strict";
+
+class TuneId {
+  constructor(name, style, mp3, pdf) {
+    this.name = name;
+    this.style = style;
+    this.mp3 = mp3;
+    this.pdf = pdf;
+  }
+}
+
+const aHalfDbl = new TuneId("A' Half Doubling", "2/4", "", "");
+
+const forty2Highlanders = new TuneId("42nd Highlander's March", "2/4", "", "");
+
+const 79Farewell = new TuneId("79th's Farewell to Gibraltar", "2/4", "", "");
+
+const amazing = new TuneId("Amazing Grace", "slowAir", "", "");
+
+const america = new TuneId("America the Beautiful", "4/4", "", "");
+
+const annie = new TuneId("Annie Grant", "strathspey", "strathspey", "", "");
+
+const arniston = new TuneId("Arniston Castle", "strathspey", "", "tunes/ArnistonCastle.pdf" );
+
+const arthur= new TuneId("Arthur Bignould", "2/4", "", "");
+
+const aspen = new TuneId("Aspen Bank", "strathspey", "", "");
+
+const waterloo = new TuneId("Battle of Waterloo", "2/4", "", "");
+
+const beverleys = new TuneId("Beverly's Wedding", "hornpipe", "", "");
+
+const biddy = new TuneId("Biddy the Bowl Wife", "jig", "", "");
+
+const birl = new TuneId("Birl", "exercises", "", "");
+
+const blackBear = new TuneId("Black Bear", "2/4", "", "");
+
+const bonnie = new TuneId("Bonnie Dundee", "2/4", "", "");
+
+const boysCork = new TuneId("Boys of Cork", "4/4", "", "");
+
+const boysWexford= new TuneId("Boys of Wexford", "4/4", "", "");
+
+const boru = new TuneId("Brian Boru's March", "6/8", "", "");
+
+const brownHairedReel = new TuneId("Brown Haired Maid (Reel)", "reel", "", "");
+
+const brownHaired2Four = new TuneId("Brown Haired Maiden (2/4 March)", "2/4", "", "");
+
+const gandyTachum = new TuneId("Bruce Gandy Tachums", "exercises", "", "");
+
+const cabarMarch = new TuneId("Cabar Feidh (March)", "4/4", "", "");
+
+const caledonian = new TuneId("Caledonian Society of London", "strathspey", "", "");
+
+const charlies = new TuneId("Charlie's Welcome", "reel", "", "");
+
+const clumsy = new TuneId("Clumsy Lover", "hornpipe", "", "");
+
+const cockerel = new TuneId("Cockerel in the Creel", "reel", "", "");
+
+const connaught = new TuneId("Connaught Man's Rambles", "jig", "", "");
+
+const corkHill = new TuneId("Cork Hill", "jig", "", "");
+
+const corkPipeband = new TuneId("Cork Pipe Band of New York City", "4/4", "", "");
+
+const dThrow = new TuneId("D Throw: Open Vs. Closed", "exercises", "", "");
+
+const danish = new TuneId("Danish Knifegrinder", "slowAir", "", "");
+
+const dannyBoy = new TuneId("Danny Boy", "slowAir", "", "");
+
+const darkIsland = new TuneId("Dark Island", "slowAir", "", "");
+
+const dawning = new TuneId("Dawning of the Day", "4/4", "", "");
+
+const donaldMac = new TuneId("Donald MacLellan of Rothesay", "2/4", "", "");
+
+const dora = new TuneId("Dora MacLeod", "strathspey", "", "");
+
+const doubleCutTachum = new TuneId("Double Cut Tachum", "exercises", "", "");
+
+const doubleCutTrachum = new TuneId("Double Cut Trachums", "exercises", "", "");
+
+const duncanMac = new TuneId("Duncan MacInnes", "2/4", "", "");
+
+const dunrovin = new TuneId("Dunrovin Farm", "jig", "", "");
+
+const eFDoublings = new TuneId("E/F Doublings", "exercises", "", "");
+
+
+const easternTown = new TuneId("Eastern Townships", "slowAir", "", "");
+
+const ewe = new TuneId("Ewe with the Crooked Horn", "strathspey", "", "");
+
+const ferryside = new TuneId("Ferryside Lassies", "jg", "", "");
+
+const frJohnMacmillan = new TuneId("Fr. John MacMillan of Barra", "2/4", "", "");
+
+const frankThompson = new TuneId("Frank Thompson", "6/8", "", "");
+
+const gallowayHills = new TuneId("Galloway Hills", "4/4", "", "");
+
+const garryowen = new TuneId( "Garryowen", "6/8", "", "");
+
+const ghillie = new TuneId("Ghillie Callum", "strathspey", "", "");
+
+const glendareul = new TuneId("Glendaruel Highlanders", "6/8", "", "");
+
+const goingHome = new TuneId("Going Home", "slowAir", "", "");
+
+const grandOldFlag = new TuneId("Grand Old Flag", "4/4", "", "");
+
+const greenHills = new TuneId("Green Hills of Tyrol", "3/4", "", "");
+
+const greenwoodside = new TuneId("Greenwoodside", "2/4", "", "");
+
+const haughs = new TuneId("Haughs of Cromdale", "4/4", "", "");
+
+const gareloch = new TuneId("High Road to Gareloch", "2/4", "", "");
+
+const linton = new TuneId("High Road to Linton", "reel", "", "");
+
+const highlandWedding = new TuneId("Highland Wedding", "2/4", "", "");
+
+const hillsKintail = new TuneId("Hills of Kintail", "2/4", "", "");
+
+const inOutHarbor = new TuneId("In and Out the Harbor", "reel", "", "");
+
+const irishSoldier = new TuneId("Irish Soldier Boy", "4/4", "", "");
+
+const jeanMauchline = new TuneId("Jean Mauchline", "6/8", "", "");
+
+const jimmyYoung = new TuneId("Jimmy Young", "2/4", "", "");
+
+const jochWilson = new TuneId("Jock Wilson's Ball", "reel", "", "");
+
+const johnKeith = new TuneId("John Keith Liang", "reel", "", "");
+
+const johnMacDonaldWelcome = new TuneId("John MacDonald's Welcome to South Uist", "2/4", "", "");
+
+const kellyKillane = new TuneId("Kelly the Boy from Killane", "4/4", "", "");
+
+
+const lachlanMc = new TuneId("Lachlan McPhail of Tiree", "reel", "", "");
+
+const ladyLever = new TuneId("Lady Lever Park", "2/4", "", "");
+
+const ladyKnock = new TuneId("Lady of Knock", "slowAir", "", "");
+
+const lamentOldSword = new TuneId("Lament for the Old Sword", "piob", "", "");
+
+const letErin = new TuneId("Let Erin Remember", "4/4", "", "");
+
+const lexyMc = new TuneId("Lexy McAskill", "reel", "", "");
+
+const lochanside = new TuneId("Lochanside", "3/4", "", "");
+
+const lordLovat = new TuneId("Lord Lovat's Lament", "slowAir", "", "");
+
+const maggieCam = new TuneId("Maggie Cameron", "strathspey", "", "");
+
+const maidsOfBlack = new TuneId("Maids of the Black Glen", "strathspey", "", "");
+
+const malts = new TuneId("Malts on the Optics", "reel", "", "");
+
+const marineCorp = new TuneId("Marine Corp Hymn", "4/4", "", "");
+
+const marquisArgyll = new TuneId("Marquis of Argyll's Salute", "piob", "", "");
+
+const maryGrant = new TuneId("Mary Grant of Lochgelly", "4/4", "", "");
+
+const masonsApron = new TuneId("Mason's Apron", "reel", "", "");
+
+const masterGExercises = new TuneId("Master 'G' Gracenote, Master Doublings", "exercises", "", "");
+
+const masterCross = new TuneId("Master Cross Noise", "exercises", "", "");
+
+const merrilyDanced = new TuneId("Merrily Danced the Quaker's Wife", "jig", "", "");
+
+const minstrelBoy = new TuneId("Minstel Boy", "4/4", "", "");
+
+const moGhileMarch = new TuneId("Mo Ghile Mear (March Version)", "4/4", "", "");
+
+const moGhileSlowAir = new TuneId("Mo Ghile Mear (Slow Air Version)", "slowAir", "", "");
+
+const mollyConnel = new TuneId("Molly Connell", "strathspey", "", "");
+
+const mollyDarlin = new TuneId("Molly Darlin'", "4/4", "", "");
+
+const mollyJig = new TuneId("Molly's Jig", "jig", "", "");
+
+const mrsDonMclean = new TuneId("Mrs. Donald McLean", "2/4", "", "");
+
+const muirOrd = new TuneId("Muir of Ord", "4/4", "", "");
+
+const nationAgain = new TuneId("Nation Once Again", "4/4", "", "");
+
+const nilSenLa = new TuneId("Nil Se'n La", "2/4", "", "");
+
+const pmCalumCampbells = new TuneId("PM Calum Campbell's Caprice", "hornpipe", "", "");
+
+const pmGeorgeAllan = new TuneId("PM George Allan", "hornpipe", "", "");
+
+const pmWillieGrey = new TuneId("Pipe Major Willie Grey's Farewell to Glasgow Police Pipeband", "2/4", "", "");
+
+const pipersCove = new TuneId("Piper's Cove", "2/4", "", "");
+
+const princeCharlesWelcome = new TuneId("Prince Charles Welcome to Lochaber", "2/4", "", "");
+
+const redHackle = new TuneId("Red Hackle Pipe Band", "4/4", "", "");
+
+const rockingBaby = new TuneId("Rocking the Baby", "jig", "", "");
+
+const rowanTree = new TuneId("Rowan Tree", "4/4", "", "");
+
+
+const scotlandBrave = new TuneId("Scotland the Brave", "4/4", "", "");
+
+const seanSouth = new TuneId("Sean South of Garryowen", "4/4", "", "");
+
+const shepherdsCrook = new TuneId("Shepherd's Crook", "strathspey", "", "");
+
+const siegeDelhi = new TuneId("Siege of Delhi", "2/4", "", "");
+
+const soundSleatStraths= new 
+TuneId("Sound of Sleat (Strathspey)", "strathspey", "", "");
+
+const soundSleatReel = new TuneId("Sound of Sleat (reel)", "reel", "", "");
+
+const spancil = new TuneId("Spancil Hill", "slowAir", "", "");
+
+const steamBoat = new TuneId("Steam Boat", "6/8", "", "");
+
+const strathspeyTriplets = new 
+TuneId("Strathspey Triplets", "exercises", "", "");
+
+const tachumsCBBAAG = new TuneId("Tachums (C/B, B/A, A/G)", "exercises", "", "");
+
+const taorCrunAMach = new TuneId("Taorluath, Crunluath, A Mach", "exercises", "", "");
+
+const taps = new TuneId("Taps", "slowAir", "", "");
+
+const tressNUist = new TuneId("Trees of North Uist", "6/8", "", "");
+
+const trippingUpStairs = new 
+TuneId("Trippin' Up the Stairs", "jig", "", "");
+
+const valleyKnock = new TuneId("Valley of Knockanuer", "slowAir", "", "");
+
+const walkingFloor = new TuneId("Walking the Floor", "jig", "", "");
+
+const wearingGreen = new TuneId("Wearing of the Green", "4/4", "", "");
+
+const whenBattlesOver = new 
+TuneId("When the Battle's Over", 3/4, "", "");
+
+const willieDavie = new TuneId("Willie Davie", "reel", "", "");
+
+const yankeeDoodle= new TuneId("Yankee Doodle Dandy", "4/4", "", "");
+
+// create a loop or an array //
+
+
+var a =5;
+
